00cffaa5786a8dab456f41c8dc66a2584f6059ae
Matt Fenwick <mfenwick100@gmail.com>
2012-02-02 16:33:37 -0500

    add id for statistics output; add jQuery framework to ease statistics contributions

--- app/scripts/statistics.js
+++ app/scripts/statistics.js
@@ -1,6 +1,14 @@
 
 function addStatistics(schedule) {
+	
     $("#refreshStatistics").click(function() {
-        // TODO complete
+        // get rid of the old content
+    	$("#statsOutput").empty();
+    	// call each of colbert's functions
+    	for(var i = 0; i < colbertsfunctions.length; i++) {
+    		f = colbertsfunctions[i];
+    		// write a new element to the ul
+    		$("#statsOutput").append("<li>" + f(schedule) + "</li>");
+    	}
     });
 }
