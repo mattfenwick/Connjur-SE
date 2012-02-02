7a863d0334048a4255e380372d555346a2cc0fe9
Matt Fenwick <mfenwick100@gmail.com>
2012-02-02 17:21:21 -0500

    add statistics functions, display output to user

--- app/scripts/statistics.js
+++ app/scripts/statistics.js
@@ -1,29 +1,36 @@
-function averagePoints(schedule) {	
-                var points = schedule.getPoints();
-                var x=0;
-                var y=0;
-		for (var i = 0; i < points.length; i++) {
-	         	x += points[i][0] ;
-                 	y += points[i][1] ;
-		}
-                avX = x/points.length;
-                avY = y/points.length;
-return [avX, avY] ;
+
+function averagePoints(schedule) {    
+    var points = schedule.getPoints();
+    var x = 0;
+    var y = 0;
+    for(var i = 0; i < points.length; i++) {
+        x += points[i][0];
+        y += points[i][1];
+    }
+    var avX = x / points.length;
+    var avY = y / points.length;
+    return [avX, avY];
+}
+
+function totalPoints(schedule) {
+	return schedule.getPoints().length;
 }
 
 
-var colbertsfunctions = [averagePoints];
+var statsFuncs = {"average coordinates": averagePoints,
+		          "total points": totalPoints};
+
 
 function addStatistics(schedule) {
-	
+
     $("#refreshStatistics").click(function() {
         // get rid of the old content
-    	$("#statsOutput").empty();
-    	// call each of colbert's functions
-    	for(var i = 0; i < colbertsfunctions.length; i++) {
-    		f = colbertsfunctions[i];
-    		// write a new element to the ul
-    		$("#statsOutput").append("<li>" + f(schedule) + "</li>");
-    	}
+        $("#statsOutput").empty();
+        // call each stat function
+        for(var key in statsFuncs) {
+            var f = statsFuncs[key];
+            // write a new element to the ul
+            $("#statsOutput").append("<li>" + key + ": " + f(schedule) + "</li>");
+        }
     });
 }
