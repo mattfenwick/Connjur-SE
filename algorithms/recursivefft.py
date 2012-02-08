4df681f7d938330ae2be2f53308845c1844b3587
Colbert Sesanker <sesanker0@gmail.com>
2012-02-08 16:26:11 -0500

    compute magnitude, and check if len(input) = 2^n

--- algorithms/recursivefft.py
+++ algorithms/recursivefft.py
@@ -1,20 +1,26 @@
 import math
 
-def fft(x):   
-    """ add a doc-string here"""
+def complex_fft(x): 
+    """ input: vector with 2^n points, outputs fft of same length"""       
     N = len(x)   
-    M = N / 2 
-    if N % 2 :  # avoid implicit boolean conversions
-        return x  # should throw exception if the input is wrong
+    M = N /2 
+    if N == 1 :                 # recursion base, test if length x is one
+        return x
+    if (N % 2):                 # using numeric output from N % 2 as boolean
+        return 0
     x_even = x[::2]      
     x_odd  = x[1::2]
     X_even = fft(x_even)
     X_odd  = fft(x_odd)
-    X = [0] * N
-    M = N / 2
+    X = [0]*N                   # Allocate Zeros
+    M = N /2
     for k in range(M) :
         twiddle = math.e ** (2j * math.pi * k / N)
         X[k] = X_even[k] + X_odd[k] * twiddle
         X[M+k] = X_even[k] - X_odd[k] * twiddle   
     return X
 
+def fft(x):
+    complex_spectrum = complex_fft(x)
+    return map(abs, complex_spectrum)
+    
