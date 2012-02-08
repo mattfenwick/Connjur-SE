51c8e7eb55276d85d675c60e95aa7f43038af653
Matt Fenwick <mfenwick@student.uchc.edu>
2012-02-07 21:46:18 -0600

    fix filename, add comments for future work to fft implementation

--- algorithms/recursivefft.py
+++ algorithms/recursivefft.py
@@ -1,16 +1,17 @@
 import math
 
 def fft(x):   
+    """ add a doc-string here"""
     N = len(x)   
-    M = N /2 
-    if N % 2 : 
-        return x
+    M = N / 2 
+    if N % 2 :  # avoid implicit boolean conversions
+        return x  # should throw exception if the input is wrong
     x_even = x[::2]      
     x_odd  = x[1::2]
     X_even = fft(x_even)
     X_odd  = fft(x_odd)
-    X = [0]*N
-    M = N /2
+    X = [0] * N
+    M = N / 2
     for k in range(M) :
         twiddle = math.e ** (2j * math.pi * k / N)
         X[k] = X_even[k] + X_odd[k] * twiddle
