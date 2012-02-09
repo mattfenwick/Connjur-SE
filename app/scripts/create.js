ecc238dc687bd5a9fa6f57bf56743ecac5f31c6e
Matt Fenwick <mfenwick100@gmail.com>
2012-02-09 17:04:00 -0500

    add event handlers for add/remove point; style for tables; tabs with spaces

--- app/scripts/create.js
+++ app/scripts/create.js
@@ -1,10 +1,10 @@
 
 function addCreate(schedule) {
-	
-	$("#createRandom").click(function() {
-		for(var i = 0; i < 100; i++) {
-	        schedule.addPoint([Math.floor(Math.random() * 100) + 1,  // so the min is 1 and the max is 100
-	                           Math.floor(Math.random() * 100) + 1]);
-		}
-	});
+    
+    $("#createRandom").click(function() {
+        for(var i = 0; i < 100; i++) {
+            schedule.addPoint([Math.floor(Math.random() * 100) + 1,  // so the min is 1 and the max is 100
+                               Math.floor(Math.random() * 100) + 1]);
+        }
+    });
 }
--- app/scripts/load.js
+++ app/scripts/load.js
@@ -2,12 +2,13 @@
 function addLoad(schedule) {
     $('#chooseSched').change(function() { 
         var myid = $('#chooseSched').val();
+        // popup a dialog saying 'loading'
         $.getJSON('/getschedule', {id: myid}, function(j) {
             // TODO dump the points into the chart
             //    is j an object or an array?
 	        var pts = j["points"];
 	        pts.map(function(pt) { schedule.addPoint(pt["location"]);} );
-	
+	        // close the dialog
         });
     });
 }
