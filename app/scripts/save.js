806dd8d22530f0ba57b5438f35714d924841dbef
Matt Fenwick <mfenwick100@gmail.com>
2012-02-09 17:02:26 -0500

    return json as 'save' response even if error; check for error client-side

--- app/scripts/save.js
+++ app/scripts/save.js
@@ -4,14 +4,20 @@ function addSave(schedule) {
 		var formatted = JSON.stringify({"points": schedule.getPoints()});
 		var auth = $("#author").val();
 		var desc = $("#description").val();
+		// popup a dialog saying 'saving' ----------------------- //////
 		var jqxhr = $.post( '/saveschedule', 
 				{'schedulestring': formatted,
 			     'author':         auth,
 			     'description':    desc}, 
 				function (response) {
-			        // maybe alert the user that it was successful
-			        alert("schedule saved successfully!" + response); // change to jqueryui widget
+			    	 var res = JSON.parse(response); //error: no parse ????
+			    	 if("success" in res) {
+			    		 // change dialogs to jqueryui
+			    		 alert("success: " + res.success);
+			    	 } else {
+			    		 alert("failed: " + res.error); // if we gave it bad data
+			    	 }
 		        })
-		  .error(function(resp) {alert("failed: " + resp);});
+		  .error(function(resp) {alert("ajax request failed: " + resp);}); // if the http request fails
 	});
 }
