1933d8d3bb65bf831ffa09c7700bfeb678a4cbf9
Matt Fenwick <mfenwick100@gmail.com>
2012-02-13 11:27:02 -0500

    add methods to Schedule prototype instead of each Schedule instance directly

--- app/scripts/schedule.js
+++ app/scripts/schedule.js
@@ -8,24 +8,24 @@ function Schedule(chart) {
     
     this.chart = chart;
     
-    // add the point to the model, AND to the chart
-    this.addPoint = function(pt) {
-    	if(this.points.length > 0) {
-    		if(this.points[0].length != pt.length) {
-    			throw new Error("point of bad dimensionality added");
-    		}
-    	}
-    	this.points.push(pt);
-    	this.chart.series[0].addPoint(pt);
-    }
-    
-    this.getPoints = function() {
-    	return this.points;
-    }
-    
-    this.setPoints = function(pts) {
-    	this.points = [];
-    	this.chart.series[0].setData([]);
-    	pts.map(function(pt) {this.addPoint(pt); });
-    }
+}
+
+Schedule.prototype.addPoint = function(pt) {
+	if(this.points.length > 0) {
+		if(this.points[0].length != pt.length) {
+			throw new Error("point of bad dimensionality added");
+		}
+	}
+	this.points.push(pt);
+	this.chart.series[0].addPoint(pt);
+}
+
+Schedule.prototype.getPoints = function() {
+	return this.points;
+}
+
+Schedule.prototype.setPoints = function(pts) {
+	this.points = [];
+	this.chart.series[0].setData([]);
+	pts.map(function(pt) {this.addPoint(pt); });
 }
