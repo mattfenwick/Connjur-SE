c417f30257cf9e8ed2cd47c0f99b6f87498e6f2b
Matt Fenwick <mfenwick100@gmail.com>
2012-02-27 16:12:00 -0500

    modularize modules; extend unit test coverage

--- app/scripts/output.js
+++ app/scripts/output.js
@@ -1,40 +1,47 @@
 
-function bruker(pts) {
-	var lines = pts.map(function(pt) {return pt.join(" ");});
-	var formatted = lines.join("\n"); // TODO is this the right newline character?
-	return formatted;
-}
-
-function varian(pts) {
-	var lines = pts.map(function(pt) {
-		decremented = pt.map(function(c) {return c - 1;});
-		return decremented.join(" ");
-	});
-	var formatted = lines.join("\n"); // TODO is this the right newline character?
-	return formatted;
-}
-
-function asJSON(pts) {
-	return JSON.stringify(pts);
-}
-
-function toolkit(pts) {
-	return bruker(pts); // TODO not right once we add in quadrature
-}
-
-formats = {
-		'Bruker':   bruker,
-		'Varian':   varian,
-		'JSON':     asJSON,
-		'Toolkit':  toolkit
-}
-
-function addOutput(schedule) {
-
-    $("#viewText").click(function() {
-    	var format = $("#outputFormat").val();
-    	var func = formats[format];
-        var formatted = func(schedule.getPoints().sort()); // TODO sort numerically
-        $("#scheduleOut").val(formatted);
-    });
+function Output($, schedule) {
+    var m = {
+        
+        bruker: function(pts) {
+            var lines = pts.map(function(pt) {return pt.join(" ");});
+            var formatted = lines.join("\n"); // TODO is this the right newline character?
+            return formatted;
+        },
+        
+        varian: function(pts) {
+            var lines = pts.map(function(pt) {
+                decremented = pt.map(function(c) {return c - 1;});
+                return decremented.join(" ");
+            });
+            var formatted = lines.join("\n"); // TODO is this the right newline character?
+            return formatted;
+        },
+        
+        asJSON: function(pts) {
+            return JSON.stringify(pts);
+        },
+        
+        toolkit: function(pts) {
+            return m.bruker(pts); // TODO not right once we add in quadrature
+        }
+    };
+    
+    var formats = {
+            'Bruker':   m.bruker,
+            'Varian':   m.varian,
+            'JSON':     m.asJSON,
+            'Toolkit':  m.toolkit
+    }
+    
+    m.addBehavior = function() {
+    
+        $("#viewText").click(function() {
+            var format = $("#outputFormat").val();
+            var func = formats[format];
+            var formatted = func(schedule.getPoints().sort()); // TODO sort numerically
+            $("#scheduleOut").val(formatted);
+        });
+    };
+    
+    return m;
 }
--- app/scripts/save.js
+++ app/scripts/save.js
@@ -1,23 +1,40 @@
 
-function addSave(schedule) {
-	$("#saveSchedule").click(function() {
-		var formatted = JSON.stringify({"points": schedule.getPoints()});
-		var auth = $("#author").val();
-		var desc = $("#description").val();
-		// popup a dialog saying 'saving' ----------------------- //////
-		var jqxhr = $.post( '/saveschedule', 
-				{'schedulestring': formatted,
-			     'author':         auth,
-			     'description':    desc}, 
-				function (response) {
-			    	 var res = JSON.parse(response); //error: no parse ????
-			    	 if("success" in res) {
-			    		 // change dialogs to jqueryui
-			    		 alert("success: " + res.success);
-			    	 } else {
-			    		 alert("failed: " + res.error); // if we gave it bad data
-			    	 }
-		        })
-		  .error(function(resp) {alert("ajax request failed: " + resp);}); // if the http request fails
-	});
+
+function Save($, schedule) {
+    var m = {
+        onSuccess: function (res) {
+            if("success" in res) {
+                // change dialogs to jqueryui
+                alert("success: " + res.success);
+            } else {
+                alert("failed: " + res.error); // if we gave it bad data
+            }
+        },
+        
+        onError: function(resp) {
+            alert("ajax http request failed: " + resp);
+        }
+    };
+    
+    m.addBehavior = function() {
+        $("#saveSchedule").click(function() {
+            var formatted = JSON.stringify({"points": schedule.getPoints()});
+            var auth = $("#author").val();
+            var desc = $("#description").val();
+            // popup a dialog saying 'saving' ----------------------- //////
+            $.ajax({
+                url:       '/saveschedule', 
+                type:      'POST',
+                dataType:  'json',
+                data:      {'schedulestring': formatted,
+                            'author':         auth,
+                            'description':    desc},
+                success:   m.onSuccess,
+                error:     m.onError,  // if the http request fails
+                timeout:   2000 // is this long enough?
+            });
+        });
+    };
+    
+    return m;
 }
